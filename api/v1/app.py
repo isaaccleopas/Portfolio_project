e940c5772e1295e759efb029bed0ed3108af2517
isaaccleopas <isaaccleopas@gmail.com>
2023-06-28 13:50:36 +0000

    ready to deploy

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,32 @@
 #!/usr/bin/python3
 """Contains a Flask web application API"""
+import json
+import requests
+from datetime import datetime
+from flask_login import login_required
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from models import storage
+from models.user import User
+from models.reservation import Reservation
+from models.review import Review
+from models.event import Event
+from flask_wtf import csrf
+from werkzeug.utils import secure_filename
 import os
 from flask import after_this_request
 from flask import Flask, jsonify
 from flask_cors import CORS
-from web_jinja.auth import signin, signup, signout, profile
+from web_jinja.routes import signin, signup, signout, profile
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__, template_folder='templates')
-app.config['SECRET_KEY'] = '575ea3040135364ec552de39befd1add'
+app.config['UPLOAD_FOLDER'] = ''
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.config['SECRET_KEY'] = os.environ.get('MY_APP_SECRET_KEY')
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
@@ -23,12 +40,6 @@ def error_404(error):
     """Handles the error code 404"""
     return jsonify(error='not found'), 404
 
-@app.after_request
-def add_cache_control(response):
-    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['Expires'] = '0'
-    return response
 
 @app.errorhandler(400)
 def error_400(error):
--- models/user.py
+++ models/user.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Contains user class"""
+import hashlib
 import models
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
-import hashlib
 from flask_login import UserMixin
 
 class User(UserMixin, BaseModel, Base):
@@ -13,14 +13,15 @@ class User(UserMixin, BaseModel, Base):
     name = Column(String(128), nullable=False)
     email = Column(String(128), unique=True, nullable=False)
     password = Column(String(100), nullable=False)
-    firebase_uid = Column(String(128), unique=True)
-    google_token = Column(String(128))
-    facebook_token = Column(String(128))
     reviews = relationship("Review", back_populates="user")
     events = relationship("Event", back_populates="user",
                           cascade="all, delete")
     reservations = relationship("Reservation", back_populates="user",
                                 cascade="all, delete")
+
+    def set_password(self, password):
+        """Sets the password for the user by hashing it"""
+        self.password = hashlib.md5(password.encode()).hexdigest()
 
     def validate_password(self, password):
         hashed_input_password = hashlib.md5(password.encode()).hexdigest()
@@ -38,10 +39,4 @@ class User(UserMixin, BaseModel, Base):
             m = hashlib.md5()
             m.update(str.encode(password))
             kwargs['password'] = m.hexdigest()
-        if "google_token" in kwargs:
-            google_token = kwargs["google_token"]
-            self.google_token = google_token
-        if "facebook_token" in kwargs:
-            facebook_token = kwargs["facebook_token"]
-            self.facebook_token = facebook_token
         super().__init__(*args, **kwargs)
